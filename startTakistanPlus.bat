6265b86df3bd25d838ea931a6ba8c364e9bdb080
Tonby73 <Tonby73@yandex.ru>
2025-02-10 08:40:12 +0400

    Added a bunch of fings, and a ton of stuff, also strictured files a bit

--- startTakistanPlus.bat
+++ startTakistanPlus.bat
@@ -16,7 +16,7 @@ title %serverName% batch
 cd "%serverLocation%"
 echo (%time%) %serverName% started.
 ::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
-start "DayZ Server" /min "DayZServer_x64.exe" -config=%serverConfig% -port=%serverPort% "-mod=@TakistanPlus;@CF;@Community-Online-Tools;@Dabs Framework;@DayZ-Expansion-Bundle;@DayZ-Expansion-Licensed;@Survivor Animations;@Takistan_world_ambient;@Takistan Clothing;@DayZ-Dog;@DayZ Horse;@Gas-Pump-Refueling;@RepaireRadiator;@RaG_Immersive_Vehicles;@Techs 4x4 Hilux;@Tactical Flava;@AirRaid;@GoreZ;@HMMWV;@CarStandFix;@MusicMod2" -profiles=profiles -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
+start "DayZ Server" /min "DayZServer_x64.exe" -config=%serverConfig% -port=%serverPort% "-mod=@TakistanPlus;@CF;@Community-Online-Tools;@Dabs Framework;@DayZ-Expansion-Bundle;@DayZ-Expansion-Licensed;@Survivor Animations;@Takistan_world_ambient;@Takistan Clothing;@DayZ-Dog;@DayZ Horse;@RepaireRadiator;@RaG_Immersive_Vehicles;@Techs 4x4 Hilux;@Tactical Flava;@AirRaid;@GoreZ;@HMMWV;@CarStandFix;@MusicMod2;@Advanced Weapon Scopes;@Doc's Car Fixes;@Arma 2 Helicopters Remastered" -profiles=profiles -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
 ::Time in seconds before kill server process (14400 = 4 hours)
 timeout 14390
 taskkill /im DayZServer_x64.exe /F
