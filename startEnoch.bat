c5f47cc6d4fa51426ec96b7a29004fb7e9bbea6e
Tonby73 <Tonby73@yandex.ru>
2025-03-26 20:59:37 +0400

    pre toxic final

---
+++ startEnoch.bat
@@ -0,0 +1,26 @@
+@echo off
+:start
+::Server name
+set serverName=Dayz_NewMag
+::Server files location
+set serverLocation="E:\SteamLibrary\steamapps\common\DayZServer"
+::Server Port
+set serverPort=2302
+::Server config
+set serverConfig=serverDZEnoch.cfg
+::Logical CPU cores to use (Equal or less than available)
+set serverCPU=2
+::Sets title for terminal (DONT edit)
+title %serverName% batch
+::DayZServer location (DONT edit)
+cd "%serverLocation%"
+echo (%time%) %serverName% started.
+::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
+start "DayZ Server" /min "DayZServer_x64.exe" -config=%serverConfig% -port=%serverPort% "-mod=@CF;@Community-Online-Tools;@Dabs Framework;@DayZ-Expansion-Bundle;@DayZ-Expansion-Licensed;@DayzReloadedServerPack;@DayZ Editor Loader;@CJ187-RandomFuelStations" -profiles=profiles -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
+::Time in seconds before kill server process (14400 = 4 hours)
+timeout 14390
+taskkill /im DayZServer_x64.exe /F
+::Time in seconds to wait before..
+timeout 0
+::Go back to the top and repeat the whole cycle again
+goto start
